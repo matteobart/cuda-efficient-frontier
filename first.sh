c1366675e6260fa35430ee6ee3caca167b0832c2
Matteo Bartalini <matteobartalini1@gmail.com>
2019-04-24 15:39:05 -0400

    Changed some supporting files

--- first.sh
+++ first.sh
@@ -1,4 +1,2 @@
 make
-./reg GOOG.csv TSLA.csv ^VIX.csv BA.csv NFLX.csv
-gnuplot -p plotinfo.txt < riskreturn.txt 
-gnuplot -p plotinfo.txt < riskreturngold.txt 
+./reg XOM.csv ^VIX.csv AMZN.csv AVGO.csv BA.csv BABA.csv BTC-USD.csv CMG.csv DIS.csv DPZ.csv EA.csv FB.csv FDX.csv GE.csv GOOG.csv IRBT.csv JPM.csv KO.csv NFLX.csv NVDA.csv PFE.csv PZZA.csv SBUX.csv SHOP.csv TMUS.csv TSCO.csv TSLA.csv TTWO.csv TWTR.csv WFC.csv WMT.csv
